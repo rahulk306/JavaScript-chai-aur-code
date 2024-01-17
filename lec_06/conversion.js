2f3968957ef79fdbeaf67bfc5c770f30863538fc
rahulk306 <139708554+rahulk306@users.noreply.github.com>
2024-01-17 20:40:29 +0000

    conversion done

---
+++ lec_06/conversion.js
@@ -0,0 +1,17 @@
+let score = "rittik"
+
+// console.log(typeof score)
+// console.log(score)
+
+let valueNumber = Number(score)
+// console.log(typeof valueNumber)
+// console.log(valueNumber)
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1
+
+let isLoggedIn = "rittik"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
