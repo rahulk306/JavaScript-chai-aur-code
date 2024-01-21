fec2e9f29eaed3b5ba20d5127e105fe563d005b1
rahulkumar306 <rahulkumar89867@gmail.com>
2024-01-21 11:51:40 +0530

    datatypes

---
+++ lec_9_datatypes_summary/1.js
@@ -0,0 +1,23 @@
+// primitive -> call by value hota hai jb kbhi bhi inko copy keya jata hai
+
+// 7 types : String, Number, Boolean, null, undefined(variable declared hai but value kon sa dalna hai ye decided nhi hai), Symbol(value ko unique bnane ke leye use krte hai), BigInt(bade data to hold krne ke leye use krte hai)
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
+
+// const id = Symbol('123')
+// console.log(id)
+// const id2 = Symbol('123')
+// console.log(id === id2) // false 
+
+// const bigNumber = 1234n
+// console.log(bigNumber)
+
+const heros = ["shaktiman", "naagraj", "doga"]
+// console.log(heros)
+let myObj = {
+    name:"rittik",
+    age:55,
+}
+console.log(myObj)
